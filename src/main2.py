aeb88d82a9a9e7c0c777788da5b6cc7429bf6b24
Gaurav <gauravpindariya@gmail.com>
2025-03-22 16:23:30 +0530

    minor changes completed

--- src/main2.py
+++ src/main2.py
@@ -18,6 +18,3 @@ blog_models.Base.metadata.create_all(bind = engine)
 app.include_router(authentication_router)
 app.include_router(blog_router)
 app.include_router(user_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host = "127.0.0.1" , port = 8000 )
